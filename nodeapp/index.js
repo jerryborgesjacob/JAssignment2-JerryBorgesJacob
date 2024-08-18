70f5e5fa581ded1463c7b6ca3deb647af7bb6f29
Jerry Borges Jacob <156135012+jerryborgesjacob@users.noreply.github.com>
2024-08-17 22:36:38 -0400

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoURI)
   //  enable CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
